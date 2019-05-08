cdad2f830bf3eb87236f3268e309af891acaea29
tychoengberink <464544@student.saxion.nl>
2019-05-08 15:11:29 +0200

    Inital Commit

---
+++ src/main/java/nl/semtech/gamelibrary/GamelibraryApplication.java
@@ -0,0 +1,13 @@
+package nl.semtech.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/semtech/gamelibrary/GamelibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.semtech.gamelibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamelibraryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
