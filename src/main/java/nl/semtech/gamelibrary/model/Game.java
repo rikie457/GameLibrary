813ac9480e2d6dd8177b217821c2fc053ac5e0cd
tychoengberink <464544@student.saxion.nl>
2019-05-22 13:27:13 +0200

    Better relation

--- src/main/java/nl/semtech/gamelibrary/model/Game.java
+++ src/main/java/nl/semtech/gamelibrary/model/Game.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 public class Game {
     private String name;
     private double price;
-    private ArrayList<Genre> genres;
 
-    public Game(String name, double price, ArrayList<Genre> genres) {
+    public Game(String name, double price) {
         this.name = name;
         this.price = price;
-        this.genres = genres;
     }
 }
--- src/main/java/nl/semtech/gamelibrary/model/Genre.java
+++ src/main/java/nl/semtech/gamelibrary/model/Genre.java
@@ -1,9 +1,13 @@
 package nl.semtech.gamelibrary.model;
 
+import java.util.ArrayList;
+
 public class Genre {
     private String name;
+    private ArrayList<Franchise> franchises;
 
-    public Genre(String name) {
+    public Genre(String name, ArrayList<Franchise> franchises) {
         this.name = name;
+        this.franchises = franchises;
     }
 }
