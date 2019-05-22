494c5c02859300005b4d4c7a820086b963eb5ee3
tychoengberink <464544@student.saxion.nl>
2019-05-22 15:21:24 +0200

    Branch

--- src/main/java/nl/semtech/gamelibrary/GameController.java
+++ src/main/java/nl/semtech/gamelibrary/GameController.java
@@ -3,6 +3,7 @@ package nl.semtech.gamelibrary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GameController {
@@ -11,5 +12,10 @@ public class GameController {
     public String showGames(Model model){
         model.addAttribute("games", GamelibraryApplication.games);
         return "showgames";
+    }
+
+    @RequestMapping("/game/add")
+    public String newGame(){
+        return "newgame";
     }
 }
