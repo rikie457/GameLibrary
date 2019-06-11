7c7922caf20cc3ed5108048b765b25381475f39a
BitSemih <464543@student.saxion.nl>
2019-06-11 13:51:54 +0200

    added styling

--- src/main/java/nl/semtech/gamelibrary/GamelibraryApplication.java
+++ src/main/java/nl/semtech/gamelibrary/GamelibraryApplication.java
@@ -34,15 +34,45 @@ public class GamelibraryApplication {
         game1.setName("Call of Duty: Black Ops 1");
         game1.setPrice(59.99);
 
-        Genre fps = new Genre();
-        fps.setName("FPS");
+        Genre genre = new Genre();
+        genre.setName("FPS");
+
+        Genre genre1 = new Genre();
+        genre1.setName("Action");
+
+        Genre genre2 = new Genre();
+        genre2.setName("Puzzle");
+
+        Genre genre3 = new Genre();
+        genre3.setName("Drama");
+
+        Genre genre4 = new Genre();
+        genre4.setName("Adventure");
+
+        Genre genre5 = new Genre();
+        genre5.setName("Simulation");
+
+        Genre genre6 = new Genre();
+        genre6.setName("Management");
 
         franchise.addGameToFranchise(game);
         franchise.addGameToFranchise(game1);
-        fps.addFranchiseToGenre(franchise);
+        genre.addFranchiseToGenre(franchise);
+        genre1.addFranchiseToGenre(franchise);
+        genre2.addFranchiseToGenre(franchise);
+        genre3.addFranchiseToGenre(franchise);
+        genre4.addFranchiseToGenre(franchise);
+        genre5.addFranchiseToGenre(franchise);
+        genre6.addFranchiseToGenre(franchise);
 
         franchises.add(franchise);
-        genres.add(fps);
+        genres.add(genre);
+        genres.add(genre1);
+        genres.add(genre2);
+        genres.add(genre3);
+        genres.add(genre4);
+        genres.add(genre5);
+        genres.add(genre6);
         games.add(game);
         games.add(game1);
     }
